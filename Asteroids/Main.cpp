428d3266d8c2372ec7704fc91d1e3e08be056ce2
VernonLu <55864722+VernonLu@users.noreply.github.com>
2019-12-03 13:55:16 -0800

    refactor: make main function looks clean

--- Asteroids/Main.cpp
+++ Asteroids/Main.cpp
@@ -22,12 +22,6 @@ sf::Vector2f winSize(1366, 768);
 sf::ContextSettings settings;
 GameState state;
 
-sf::Vector2f btnSize(240, 80);
-
-sf::Font font;
-
-sf::Sprite bg;
-
 /*Texture*/
 sf::Texture aircraftTex;
 sf::Texture backgroundTex;
@@ -43,14 +37,29 @@ sf::Sound bgm(bgmBuffer);
 
 sf::SoundBuffer thrustBuffer;
 
-/*Text*/
+/*UI*/
+//Font
+sf::Font font;
+//Background
+sf::Sprite bg;
+//Text
 sf::Text debug;
-Score score;
-
-/*Button*/
+int score;
+sf::Text ingameScore;
+sf::Text endGameScore;
+//Button
+sf::Vector2f btnSize(240, 80);
 std::vector<Button*> menuBtnList;
 Button startBtn;
+//Health Bar
+HealthBar healthBar(3);
 
+/*GameObjects*/
+
+std::vector<GameObject*> bucket;
+Aircraft player;
+std::vector<Bullet*> bulletPool;
+std::vector<Asteroid*> asteroidPool;
 
 bool LoadResources() {
 	//Load font
@@ -59,7 +68,7 @@ bool LoadResources() {
 	//Load texture
 	if (!aircraftTex.loadFromFile("resources/Textures/aircraft_player.png")) { return false; }
 	if (!backgroundTex.loadFromFile("resources/Textures/texture_background.jpg")) { return false; }
-	if (!btnTex.loadFromFile("resources/Textures/texture_button.png")) { return false; }
+	if (!btnTex.loadFromFile("resources/Textures/button.png")) { return false; }
 	if (!health.loadFromFile("resources/Textures/heart.png")) { return false; }
 	if (!healthBg.loadFromFile("resources/Textures/heart_bg.png")) { return false; }
 	if (!bulletTex.loadFromFile("resources/Textures/bullet.png")) { return false; }
@@ -72,38 +81,78 @@ bool LoadResources() {
 	return true;
 }
 
-void Setupbackground(){
-	bg.setTexture(backgroundTex);
-}
-
 void StartGame() {
 	state = GameState::STATE_GAME;
 }
 
+void Init() {
+
+	bg.setTexture(backgroundTex);
+
+	debug.setFont(font);
+	debug.setCharacterSize(40);
+	debug.setPosition(1, 1);
+
+	startBtn.SetSize(sf::Vector2f(240, 80));
+	startBtn.SetTexture(btnTex);
+	startBtn.SetClickEvent(&StartGame);
+	startBtn.SetPosition(winSize / 2.f);
+	startBtn.SetCaption(font, "Start", 40);
+
+
+	healthBar.SetTexture(health);
+	healthBar.SetBgTexture(healthBg);
+	healthBar.SetSize(60, 20);
+	healthBar.SetPosition(1, 1);
+
+
+	ingameScore.setFont(font);
+	ingameScore.setCharacterSize(40);
+	ingameScore.setPosition(10, 80);
+
+
+	player.SetTexture(aircraftTex);
+	player.SetThrustSound(thrustBuffer);
+	player.SetBulletPool(&bulletPool);
+
+}
 
 void Start() {
 	bgm.play();
 	bgm.setLoop(true);
 }
 
-void LoadLevel() {
+void LoadLevel(int index) {
+	for (int i = 0; i < 10; ++i) {
+		Asteroid* a = new Asteroid();
+		a->SetTexture(asteroidTex);
+		a->SetPosition(sf::Vector2f(rand() % 1366, (rand() % 768)));
+		a->SetDirection(rand() % 10, rand() % 10);
+		a->speed = rand() % 20 + 50;
+		a->boundary = winSize;
+		a->rotateSpeed = (rand() % 10 + 30);
+		asteroidPool.push_back(a);
+	}
+}
 
+void SpawnPlayer() {
+
+	for (int i = 0; i < 5000; ++i) {
+		Bullet* bullet = new Bullet();
+		bullet->SetTexture(bulletTex);
+		bulletPool.push_back(bullet);
+	}
+
+	player.SetPosition(winSize / 2.f);
 }
 
 int main() {
-
-	std::vector<GameObject*> bucket;
-
-
 	bool gameStart = false;
 	bool loadLevel = false;
 	bool respawn = false;
 	bool pause = false;
-	state = GameState::STATE_MENU;
 
-	debug.setFont(font);
-	debug.setCharacterSize(40);
-	debug.setPosition(1, 1);
+	state = GameState::STATE_MENU;
 
 	settings.antialiasingLevel = 8;
 	
@@ -111,46 +160,7 @@ int main() {
 	
 	if (!LoadResources()) { return EXIT_FAILURE; }
 
-	std::vector<Asteroid*> largeAsteroid;
-	for (int i = 0; i < 10; ++i) {
-		Asteroid* a = new Asteroid();
-		a->SetTexture(asteroidTex);
-		a->SetPosition(sf::Vector2f(rand() % 1366, (rand() % 768)));
-		a->SetDirection(rand() % 10, rand() % 10);
-		a->speed = rand() % 20 + 100;
-		a->boundary = winSize;
-		a->rotateSpeed = (rand() % 10 + 30);
-		largeAsteroid.push_back(a);
-	}
-
-	bg.setTexture(backgroundTex);
-	
-	startBtn.SetSize(sf::Vector2f(240, 80));
-	startBtn.SetTexture(btnTex);
-	startBtn.SetClickEvent(&StartGame);
-	startBtn.SetPosition(winSize / 2.f);
-	startBtn.SetCaption(font, "Start", 40);
-
-	std::vector<Bullet*> bulletPool;
-	for (int i = 0; i < 5000; ++i) {
-		Bullet* bullet = new Bullet();
-		bullet->SetTexture(bulletTex);
-		bulletPool.push_back(bullet);
-	}
-
-	Aircraft player;
-	player.SetPosition(winSize / 2.f);
-	player.SetTexture(aircraftTex);
-	player.SetThrustSound(thrustBuffer);
-	player.SetBulletPool(&bulletPool);
-
-	HealthBar healthBar(3);
-	healthBar.SetTexture(health);
-	healthBar.SetBgTexture(healthBg);
-	healthBar.SetSize(60, 20);
-	healthBar.SetPosition(1, 1);
-
-
+	Init();
 
 	sf::Clock clock;
 
@@ -166,6 +176,7 @@ int main() {
 
 		window.clear();
 
+		window.draw(bg);
 		switch (state) {
 		case GameState::STATE_MENU: {
 			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
@@ -179,9 +190,6 @@ int main() {
 			/*GameSetup*/
 			if (!gameStart) {
 				Start();
-				score.setFont(font);
-				score.setCharacterSize(40);
-				score.setPosition(10, 80);
 				gameStart = true;
 				loadLevel = false;
 			}
@@ -192,7 +200,7 @@ int main() {
 			/*Update*/
 			if (!pause) {
 				player.Update(deltaTime);
-				for (auto asteroid : largeAsteroid) {
+				for (auto asteroid : asteroidPool) {
 					asteroid->Update(deltaTime);
 				}
 
@@ -205,17 +213,14 @@ int main() {
 					if (!bullet->enable) { continue; }
 					bullet->Update(deltaTime);
 				}
-
-
-
 				/*Collision Detection*/
-				for (int i = 0; i < largeAsteroid.size(); ++i) {
-					for (int j = i + 1; j < largeAsteroid.size(); ++j) {
-						sf::Vector2f diff = largeAsteroid[i]->position - largeAsteroid[j]->position;
-						float r2 = pow((largeAsteroid[i]->radius + largeAsteroid[j]->radius), 2);
+				for (int i = 0; i < asteroidPool.size(); ++i) {
+					for (int j = i + 1; j < asteroidPool.size(); ++j) {
+						sf::Vector2f diff = asteroidPool[i]->position - asteroidPool[j]->position;
+						float r2 = pow((asteroidPool[i]->radius + asteroidPool[j]->radius), 2);
 						if (pow(diff.x, 2) + pow(diff.y, 2) <= r2) {
-							largeAsteroid[i]->Collide(*largeAsteroid[j]);
-							largeAsteroid[j]->Collide(*largeAsteroid[i]);
+							asteroidPool[i]->Collide(*asteroidPool[j]);
+							asteroidPool[j]->Collide(*asteroidPool[i]);
 						}
 					}
 				}
@@ -229,9 +234,8 @@ int main() {
 			
 
 			/*Render*/
-			window.draw(bg);
 
-			for (auto asteroid : largeAsteroid) {
+			for (auto asteroid : asteroidPool) {
 				if(asteroid->enable)
 					window.draw(*asteroid);
 			}
@@ -243,7 +247,8 @@ int main() {
 
 			window.draw(healthBar);
 
-			window.draw(score);
+			ingameScore.setString(std::to_string(score));
+			window.draw(ingameScore);
 
 			/*GameOver*/
 			if (healthBar.isEmpty()) {
@@ -252,6 +257,8 @@ int main() {
 		} break;
 		case GameState::STATE_OVER: {
 
+			endGameScore.setString(std::to_string(score));
+			window.draw(endGameScore);
 		} break;
 		default:
 			break;
